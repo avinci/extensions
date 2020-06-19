4356cdaacc0d991ae3f17ae2f315e1a0e6fc2ea3
Avi Cavale <avic@jfrog.com>
2020-06-19 06:58:32 -0700

    adding a new step

--- steps/sample/0.0.1/onExecute.sh
+++ steps/sample/0.0.1/onExecute.sh
@@ -1,6 +1,8 @@
 sample() {
   local downloadURL=$(find_step_configuration_value "downloadURL")
   echo "$step_name's downloadURL value is $downloadURL"
+  curl $downloadURL --output temp.zip
+  ls -al
 }
 
 execute_command sample
