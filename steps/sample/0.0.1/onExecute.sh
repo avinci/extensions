dacbac0baaf07b0d4ef44b97caec827ac1c61be7
Avi Cavale <avic@jfrog.com>
2020-06-19 13:12:34 -0700

    changing extn code

--- steps/sample/0.0.1/onExecute.sh
+++ steps/sample/0.0.1/onExecute.sh
@@ -1,8 +1,10 @@
-sample() {
+# 0.0.1  onExecute.sh
+business_logic() {
   local downloadURL=$(find_step_configuration_value "downloadURL")
+  local downloadPath=$(find_step_configuration_value "downloadPath")
   echo "$step_name's downloadURL value is $downloadURL"
-  curl $downloadURL --output temp.zip
-  ls -al
+  echo "$step_name's downloadPath value is $downloadPath"
+  curl $downloadURL --output $downloadPath/temp.zip
+  ls -al $downloadPath
 }
-
-execute_command sample
+execute_command business_logic
