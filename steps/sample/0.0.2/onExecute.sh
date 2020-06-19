f6a8300806a72523f9eaa76db016292ec6e753ce
Avi Cavale <avic@jfrog.com>
2020-06-19 13:35:46 -0700

    adding 0.0.2

--- steps/sample/0.0.2/onExecute.sh
+++ steps/sample/0.0.2/onExecute.sh
@@ -1,8 +1,10 @@
 # 0.0.2  onExecute.sh
 business_logic() {
   local downloadURL=$(find_step_configuration_value "downloadURL")
+  local downloadPath=$(find_step_configuration_value "downloadPath")
+  local doUnzip=$(find_step_configuration_value "doUnzip")
   echo "$step_name's downloadURL value is $downloadURL"
-  echo "$step_name's downloadURL value is $downloadURL"
+  echo "$step_name's downloadPath value is $downloadPath"
   echo "$step_name's doUnzip value is $doUnzip"
   mkdir -p $downloadPath
   curl $downloadURL --output $downloadPath/temp.zip
